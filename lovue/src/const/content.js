7449f0b0ec31e8a3bbd9c564eec622bcdf03f875
ss <shenxin@magmeng.com>
2020-03-07 22:46:43 +0800

    认领 demo: for 列表渲染

--- lovue/src/const/content.js
+++ lovue/src/const/content.js
@@ -1,290 +1,290 @@
 export default {
     lists: [
         {
-            title: '基础',
+            title: "基础",
             datas: [
                 {
-                    title: 'Vue 实例',
+                    title: "Vue 实例",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '模板语法',
+                    title: "模板语法",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '计算属性和监听器',
+                    title: "计算属性和监听器",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: 'Class 与 Style 绑定',
+                    title: "Class 与 Style 绑定",
                     author: {
-                        nickname: 'Wynne',
-                        avatar: 'https://avatars1.githubusercontent.com/u/20253837?s=460&v=4',
-                        link: 'https://www.yuque.com/wynne-xt3c9/dbpoxp/tvx62s'
+                        nickname: "Wynne",
+                        avatar: "https://avatars1.githubusercontent.com/u/20253837?s=460&v=4",
+                        link: "https://www.yuque.com/wynne-xt3c9/dbpoxp/tvx62s"
                     }
                 },
                 {
-                    title: 'if 条件渲染',
+                    title: "if 条件渲染",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: 'for 列表渲染',
+                    title: "for 列表渲染",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "沈鑫",
+                        avatar: "https://avatars2.githubusercontent.com/u/28528399?s=60&v=4",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '事件处理',
+                    title: "事件处理",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '表单输入绑定',
+                    title: "表单输入绑定",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 }
             ]
         },
         {
-            title: '组件',
+            title: "组件",
             datas: [
                 {
-                    title: '组件基础',
+                    title: "组件基础",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '组件注册',
+                    title: "组件注册",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: 'Prop'
+                    title: "Prop"
                 },
                 {
-                    title: '自定义事件',
+                    title: "自定义事件",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '插槽',
+                    title: "插槽",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '动态组件',
+                    title: "动态组件",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '异步组件',
+                    title: "异步组件",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '处理边界情况',
+                    title: "处理边界情况",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 }
             ]
         },
         {
-            title: '过渡 & 动画',
+            title: "过渡 & 动画",
             datas: [
                 {
-                    title: '进入/离开 & 列表过渡',
+                    title: "进入/离开 & 列表过渡",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '状态固定',
+                    title: "状态固定",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 }
             ]
         },
         {
-            title: '可复用性 & 组合',
+            title: "可复用性 & 组合",
             datas: [
                 {
-                    title: '混入',
+                    title: "混入",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '自定义指令',
+                    title: "自定义指令",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '渲染函数 & JSX',
+                    title: "渲染函数 & JSX",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '插件',
+                    title: "插件",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '过滤器',
+                    title: "过滤器",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 }
             ]
         },
         {
-            title: '工具',
+            title: "工具",
             datas: [
                 {
-                    title: '单文件组件',
+                    title: "单文件组件",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '单元测试',
+                    title: "单元测试",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: 'TypeScript支持',
+                    title: "TypeScript支持",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '生产环境部署',
+                    title: "生产环境部署",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 }
             ]
         },
         {
-            title: '规模化',
+            title: "规模化",
             datas: [
                 {
-                    title: '路由',
+                    title: "路由",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '状态管理',
+                    title: "状态管理",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '服务端渲染',
+                    title: "服务端渲染",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 },
                 {
-                    title: '安全',
+                    title: "安全",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 }
             ]
         },
         {
-            title: '原理',
+            title: "原理",
             datas: [
                 {
-                    title: '深入响应原理',
+                    title: "深入响应原理",
                     author: {
-                        nickname: '未认领',
-                        avatar: 'https://cn.vuejs.org/images/logo.png',
-                        link: '../assets/logo.png'
+                        nickname: "未认领",
+                        avatar: "https://cn.vuejs.org/images/logo.png",
+                        link: "../assets/logo.png"
                     }
                 }
             ]
-        },
+        }
     ]
-}
+};
