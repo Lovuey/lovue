094ed7a3ce9fa2c513d27442b8a46d68df5c7e33
张晓檬 <921996026@qq.com>
2020-03-13 00:43:54 +0800

    Prop认领

--- lovue/src/const/content.js
+++ lovue/src/const/content.js
@@ -100,11 +100,11 @@ export default {
                 },
                 {
                     title: "Prop",
-                    des: "请负责人人添加描述，最多 30 个字。",
+                    des: "子组件接受父组件传递数据的一个自定义属性",
                     author: {
-                        nickname: "未认领",
-                        avatar: "https://cn.vuejs.org/images/logo.png",
-                        link: "../assets/logo.png"
+                        nickname: "张晓檬",
+                        avatar: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584014362336&di=0cfe72d3caadf019fd13c0a9b327e9d5&imgtype=0&src=http%3A%2F%2Fwww.deskcar.com%2Fdesktop%2Fcartoon%2Fjapan%2F2011417214141%2F22.jpg",
+                        link: "https://github.com/zhangxiaomeng1"
                     }
                 },
                 {
