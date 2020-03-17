632aa5c0534234c007f5ddd58da9bf5702307c9e
jarbo.zhang <jarbo.zhang@gmail.com>
2020-03-17 18:52:59 +0800

    添加header菜单和footer

--- lovue/src/components/top.vue
+++ lovue/src/components/top.vue
@@ -2,6 +2,14 @@
     <div class="top-box">
         <img class="logo-icon" src="../assets/logo.png" alt=""><span class="logo-title">Lovue</span>
         <h1 class="nav-title">{{ msg }}</h1>
+        <div class="nav">
+            <a class="nav-link" href='/about' >
+                <img class="nav-icon" src="../assets/icon/about.png" /><span>关于</span>
+            </a>
+            <a class="nav-link" href='https://github.com/Lovuey/lovue' target="_blank">
+                <img class="nav-icon" src="../assets/icon/github.png" /><span>GITHUB</span>
+            </a>
+        </div>
     </div>
 </template>
 
@@ -43,5 +51,27 @@
     .logo-title {
         padding-left: 8px;
         font-size: 18px;
+    }
+    .nav {
+        font-size: 16px;
+        margin: 3px 40px 0 0;
+        flex-grow: 0;
+        display: flex;
+    }
+    .nav-icon {
+        width: 20px;
+        height: 20px;
+        margin: 0 5px;
+    }
+    .nav-link {
+        margin: 0 5px;
+        white-space: nowrap;
+        text-decoration: none;
+        color: #2c3e50;
+        display: flex;
+        border-bottom: 3px solid rgba(255,255,255,0);
+    }
+    .nav-link:hover {
+        border-bottom: 3px solid #42b983;
     }
 </style>
